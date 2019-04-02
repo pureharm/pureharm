cf78e94c6bba8a608408cbb63f600be390ec08a5
Lorand Szakacs <lsz@lorandszakacs.com>
2019-04-03 01:05:18 +0300

    Fix PhantomType aliasing problem
    In client code, phantom type objects defined in a trait wound up being a path dependent type

--- db-slick/src/main/scala/busymachines/pureharm/dbslick/package.scala
+++ db-slick/src/main/scala/busymachines/pureharm/dbslick/package.scala
@@ -1,6 +1,6 @@
 package busymachines.pureharm
 
-import busymachines.pureharm.dbslick.types.DBSlickTypes
+import busymachines.pureharm.core.PhantomType
 
 /**
   *
@@ -8,4 +8,19 @@ import busymachines.pureharm.dbslick.types.DBSlickTypes
   * @since 02 Apr 2019
   *
   */
-package object dbslick extends DBSlickTypes
+package object dbslick {
+  final type ConnectionIO[T] = slick.dbio.DBIO[T]
+  final val ConnectionIO: slick.dbio.DBIO.type = slick.dbio.DBIO
+
+  final type SlickDB  = slick.jdbc.JdbcProfile#Backend#Database
+  final type SlickAPI = slick.jdbc.JdbcProfile#API
+
+  final object JDBCUrl extends PhantomType[String]
+  final type JDBCUrl = JDBCUrl.Type
+
+  final object DBUsername extends PhantomType[String]
+  final type DBUsername = DBUsername.Type
+
+  final object DBPassword extends PhantomType[String]
+  final type DBPassword = DBPassword.Type
+}
