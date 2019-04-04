3cfb025fd4cdb6e844fe777620be83e9ecdb35d1
Lorand Szakacs <lsz@lorandszakacs.com>
2019-04-04 15:52:26 +0300

    [db-slick] Add cats.MonadError[ConnectionIO, Throwable] for DBIIO
    Syntax and instances available by importing:
    import busymachines.pureharm.dbslick.implicits._

---
+++ db-slick/src/main/scala/busymachines/pureharm/dbslick/implicits.scala
@@ -0,0 +1,11 @@
+package busymachines.pureharm.dbslick
+
+import busymachines.pureharm.dbslick.types.ConnectionIOCatsInstances
+
+/**
+  *
+  * @author Lorand Szakacs, https://github.com/lorandszakacs
+  * @since 04 Apr 2019
+  *
+  */
+object implicits extends ConnectionIOCatsInstances
