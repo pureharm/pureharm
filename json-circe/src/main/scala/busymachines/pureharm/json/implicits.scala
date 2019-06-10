ecac77e3dd794e787d5840960acc7948c8c2aaaf
Lorand Szakacs <lsz@lorandszakacs.com>
2019-06-11 00:35:37 +0300

    [json] Create busymachines.pureharm.json

---
+++ json-circe/src/main/scala/busymachines/pureharm/json/implicits.scala
@@ -0,0 +1,11 @@
+package busymachines.pureharm.json
+
+import busymachines.pureharm.json_impl
+
+/**
+  *
+  * @author Lorand Szakacs, https://github.com/lorandszakacs
+  * @since 11 Jun 2019
+  *
+  */
+object implicits extends json_impl.PureharmJsonSyntax.Implicits
---
+++ json-circe/src/main/scala/busymachines/pureharm/json/package.scala
@@ -0,0 +1,9 @@
+package busymachines.pureharm
+
+/**
+  *
+  * @author Lorand Szakacs, https://github.com/lorandszakacs
+  * @since 11 Jun 2019
+  *
+  */
+package object json extends busymachines.pureharm.json_impl.PureharmJsonTypeDefinitions
