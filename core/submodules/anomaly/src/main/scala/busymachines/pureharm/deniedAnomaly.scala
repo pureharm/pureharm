2471d4c981e9194e135d271b6af445ad94672460
Lorand Szakacs <lsz@lorandszakacs.com>
2019-06-10 23:23:50 +0300

    Add denied Anomaly

---
+++ core/submodules/anomaly/src/main/scala/busymachines/pureharm/deniedAnomaly.scala
@@ -0,0 +1,68 @@
+/**
+  * Copyright (c) 2017-2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package busymachines.pureharm
+
+import busymachines.pureharm.Anomaly.Parameters
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
+  * @since 10 Jun 2019
+  *
+  */
+abstract class DeniedAnomaly(
+  override val message: String,
+  causedBy:             Option[Throwable] = None,
+) extends Anomaly(message, causedBy) with MeaningfulAnomalies.Denied with Product with Serializable {
+  override def id: AnomalyID = DeniedAnomalyID
+}
+
+object DeniedAnomaly
+    extends DeniedAnomaly(MeaningfulAnomalies.DeniedMsg, None) with SingletonAnomalyProduct
+    with AnomalyConstructors[DeniedAnomaly] {
+  override def apply(id: AnomalyID): DeniedAnomaly =
+    DeniedAnomalyImpl(id = id)
+
+  override def apply(message: String): DeniedAnomaly =
+    DeniedAnomalyImpl(message = message)
+
+  override def apply(parameters: Parameters): DeniedAnomaly =
+    DeniedAnomalyImpl(parameters = parameters)
+
+  override def apply(id: AnomalyID, message: String): DeniedAnomaly =
+    DeniedAnomalyImpl(id = id, message = message)
+
+  override def apply(id: AnomalyID, parameters: Parameters): DeniedAnomaly =
+    DeniedAnomalyImpl(id = id, parameters = parameters)
+
+  override def apply(message: String, parameters: Parameters): DeniedAnomaly =
+    DeniedAnomalyImpl(message = message, parameters = parameters)
+
+  override def apply(id: AnomalyID, message: String, parameters: Parameters): DeniedAnomaly =
+    DeniedAnomalyImpl(id = id, message = message, parameters = parameters)
+
+  override def apply(a: Anomaly): DeniedAnomaly =
+    DeniedAnomalyImpl(id = a.id, message = a.message, parameters = a.parameters)
+}
+
+final private[pureharm] case class DeniedAnomalyImpl(
+  override val id:         AnomalyID         = DeniedAnomalyID,
+  override val message:    String            = MeaningfulAnomalies.DeniedMsg,
+  override val parameters: Parameters        = Parameters.empty,
+  causedBy:                Option[Throwable] = None,
+) extends DeniedAnomaly(message, causedBy) with Product with Serializable
