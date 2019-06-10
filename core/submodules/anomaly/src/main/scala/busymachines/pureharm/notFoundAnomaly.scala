640069b646842dedf261b37f329576090fb5beb3
Lorand Szakacs <lsz@lorandszakacs.com>
2019-06-10 23:24:25 +0300

    [anomaly] Add Not Found Anomaly

---
+++ core/submodules/anomaly/src/main/scala/busymachines/pureharm/notFoundAnomaly.scala
@@ -0,0 +1,71 @@
+/**
+  * Copyright (c) 2017-2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package busymachines.pureharm
+
+import busymachines.pureharm.Anomaly.Parameters
+
+/**
+  *
+  * @author Lorand Szakacs, lsz@lorandszakacs.com, lorand.szakacs@busymachines.com
+  * @since 10 Jun 2019
+  *
+  */
+
+abstract class NotFoundAnomaly(
+  override val message: String,
+  causedBy:             Option[Throwable] = None,
+) extends Anomaly(message, causedBy) with MeaningfulAnomalies.NotFound with Product with Serializable {
+  override def id: AnomalyID = NotFoundAnomalyID
+}
+
+object NotFoundAnomaly
+    extends NotFoundAnomaly(MeaningfulAnomalies.NotFoundMsg, None) with SingletonAnomalyProduct
+    with AnomalyConstructors[NotFoundAnomaly] {
+
+  override def apply(id: AnomalyID): NotFoundAnomaly =
+    NotFoundAnomalyImpl(id = id)
+
+  override def apply(message: String): NotFoundAnomaly =
+    NotFoundAnomalyImpl(message = message)
+
+  override def apply(parameters: Parameters): NotFoundAnomaly =
+    NotFoundAnomalyImpl(parameters = parameters)
+
+  override def apply(id: AnomalyID, message: String): NotFoundAnomaly =
+    NotFoundAnomalyImpl(id = id, message = message)
+
+  override def apply(id: AnomalyID, parameters: Parameters): NotFoundAnomaly =
+    NotFoundAnomalyImpl(id = id, parameters = parameters)
+
+  override def apply(message: String, parameters: Parameters): NotFoundAnomaly =
+    NotFoundAnomalyImpl(message = message, parameters = parameters)
+
+  override def apply(id: AnomalyID, message: String, parameters: Parameters): NotFoundAnomaly =
+    NotFoundAnomalyImpl(id = id, message = message, parameters = parameters)
+
+  //we intentionally not pass a causedBy a.asThrowable. Not really meaningful in this case
+  override def apply(a: Anomaly): NotFoundAnomaly =
+    NotFoundAnomalyImpl(id = a.id, message = a.message, parameters = a.parameters)
+}
+
+final private[pureharm] case class NotFoundAnomalyImpl(
+  override val id:         AnomalyID         = NotFoundAnomalyID,
+  override val message:    String            = MeaningfulAnomalies.NotFoundMsg,
+  override val parameters: Parameters        = Parameters.empty,
+  causedBy:                Option[Throwable] = None,
+) extends NotFoundAnomaly(message, causedBy) with Product with Serializable
