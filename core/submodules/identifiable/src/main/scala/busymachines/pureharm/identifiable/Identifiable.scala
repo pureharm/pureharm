e9e78f1e58ae806ec64a69d5af647105cd1860db
Lorand Szakacs <lsz@lorandszakacs.com>
2019-06-13 17:18:26 +0300

    [identifiable] Redo identifiable

--- core/submodules/identifiable/src/main/scala/busymachines/pureharm/identifiable/Identifiable.scala
+++ core/submodules/identifiable/src/main/scala/busymachines/pureharm/identifiable/Identifiable.scala
@@ -15,15 +15,29 @@
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
-package busymachines.pureharm
+package busymachines.pureharm.identifiable
+
+import busymachines.pureharm.internals.identifiable.IdentifiableLowPriorityImplicits
+
+import scala.annotation.implicitNotFound
 
 /**
   *
+  * @tparam T
+  *   the type
+  * @tparam ID
+  *   the value by which our value of type ``T`` can be uniquely identified
   * @author Lorand Szakacs, https://github.com/lorandszakacs
   * @since 04 Apr 2019
   *
   */
-object fieldname {
-  object FieldName extends PhantomType[String]
-  type FieldName = FieldName.Type
+@implicitNotFound(
+  "If a case class T, has a field called 'id of type ID then an Identifiable[T, ID] will be generated for case class, otherwise, please provide one",
+)
+trait Identifiable[T, ID] {
+  def id(t: T): ID
+
+  def fieldName: FieldName
 }
+
+object Identifiable extends IdentifiableLowPriorityImplicits
