3f9397c66b3b2fcb9f8ee149499482457f93ec80
Lorand Szakacs <lsz@lorandszakacs.com>
2019-06-15 14:45:35 +0300

    [effects] Bundle convenience methods for pool creation in one place

---
+++ effects-cats/src/main/scala/busymachines/pureharm/effects/pools/Pools.scala
@@ -0,0 +1,53 @@
+/**
+  * Copyright (c) 2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package busymachines.pureharm.effects.pools
+
+import cats.effect._
+import scala.concurrent.ExecutionContext
+
+/**
+  *
+  * @author Lorand Szakacs, https://github.com/lorandszakacs
+  * @since 15 Jun 2019
+  *
+  */
+object Pools {
+
+  def mainIOContextShift[F[_]: Sync](threadNamePrefix: String = "main-cpu-fixed"): Resource[F, ContextShift[IO]] =
+    mainContextShiftPool(threadNamePrefix).map(ec => IO.contextShift(ec))
+
+  def mainContextShiftPool[F[_]: Sync](threadNamePrefix: String = "main-cpu-fixed"): Resource[F, ExecutionContext] =
+    PoolMainCPU.default(threadNamePrefix)
+
+  def fixed[F[_]: Sync](
+    threadNamePrefix: String = "fixed",
+    maxThreads:       Int,
+    daemons:          Boolean = false,
+  ): Resource[F, ExecutionContext] =
+    PoolFixed.fixed(threadNamePrefix, maxThreads, daemons)
+
+  def cached[F[_]: Sync](threadNamePrefix: String = "cached", daemons: Boolean = false): Resource[F, ExecutionContext] =
+    PoolCached.cached(threadNamePrefix, daemons)
+
+  def singleThreaded[F[_]: Sync](
+    threadNamePrefix: String  = "single-thread",
+    daemons:          Boolean = false,
+  ): Resource[F, ExecutionContext] =
+    PoolFixed.fixed(threadNamePrefix, 1, daemons)
+
+}
