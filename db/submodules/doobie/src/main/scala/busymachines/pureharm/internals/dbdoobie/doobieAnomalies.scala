ca1686b7e9187ed12ae4a6ce2e5217af05e302ce
Lorand Szakacs <lsz@lorandszakacs.com>
2020-06-26 17:46:51 +0300

    [db-doobie] Shuffle anomalies around

---
+++ db/submodules/doobie/src/main/scala/busymachines/pureharm/internals/dbdoobie/doobieAnomalies.scala
@@ -0,0 +1,35 @@
+/**
+  * Copyright (c) 2017-2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package busymachines.pureharm.internals.dbdoobie
+
+import busymachines.pureharm.db._
+
+final private[dbdoobie] case class DoobieDBEntryNotFoundAnomaly(
+  override val pk:       String,
+  override val causedBy: Option[Throwable],
+) extends DBEntryNotFoundAnomaly(pk, causedBy)
+
+final private[dbdoobie] case class DoobieDBBatchInsertFailedAnomaly(
+  override val expectedSize: Int,
+  override val actualSize:   Int,
+  override val causedBy:     Option[Throwable],
+) extends DBBatchInsertFailedAnomaly(expectedSize, actualSize, causedBy)
+
+final private[dbdoobie] case class DoobieDBDeleteByPKFailedAnomaly(
+  override val pk: String
+) extends DBDeleteByPKFailedAnomaly(pk)
