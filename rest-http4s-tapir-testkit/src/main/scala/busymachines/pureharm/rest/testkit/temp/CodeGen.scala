7946fbad26b1e72c5546bea8016fd21c534d819a
Lorand Szakacs <lorand.szakacs@gmail.com>
2020-07-11 00:08:17 +0300

    [rest] WIP add compiler stress test for vanilla http4s

--- rest-http4s-tapir-testkit/src/main/scala/busymachines/pureharm/rest/testkit/temp/CodeGen.scala
+++ rest-http4s-tapir-testkit/src/main/scala/busymachines/pureharm/rest/testkit/temp/CodeGen.scala
@@ -8,7 +8,7 @@ import busymachines.pureharm.testkit.PureharmTest
   */
 class CodeGen extends PureharmTest {
   import busymachines.pureharm.effects._
-  val caseClasses = 50
+  val caseClasses = 100
   test("data") {
     IO {
       println("""
@@ -30,6 +30,18 @@ class CodeGen extends PureharmTest {
                 |
                 |""".stripMargin)
       println(List.range(1, caseClasses).map(tapir).mkString("\n\n"))
+    }.as(succeed)
+  }
+
+  test("http4s") {
+    IO {
+      println("""
+                |package busymachines.pureharm.rest.temp.stress
+                |
+                |import busymachines.pureharm.rest.temp._
+                |
+                |""".stripMargin)
+      println(List.range(1, caseClasses).map(http4sPlain).mkString("\n\n"))
     }.as(succeed)
   }
 
@@ -95,4 +107,36 @@ class CodeGen extends PureharmTest {
        |//---------------------------------
        |""".stripMargin
 
+  def http4sPlain(i: Int): String =
+    s"""
+      |//--------------------------------------------
+      |object TempHttp4sClassic$i {
+      |  import busymachines.pureharm.rest._
+      |  import busymachines.pureharm.rest.implicits._
+      |
+      |  import org.http4s.dsl.Http4sDsl
+      |  import busymachines.pureharm.effects._
+      |  import busymachines.pureharm.effects.implicits._
+      |
+      |  def service[F[_]: Sync]: HttpRoutes[F] = {
+      |    val dsl: Http4sDsl[F] = Http4sDsl[F]
+      |    import dsl._
+      |    HttpRoutes.of[F] {
+      |      case GET -> Root / "test" => Ok(Sync[F].delay(??? : MyOutputType$i))
+      |
+      |      case req @ POST -> Root / path =>
+      |        Ok {
+      |          for {
+      |            _      <- req.as[MyInputType$i]
+      |            _      <- path.pure[F]
+      |            result <- (??? : MyOutputType$i).pure[F]
+      |          } yield result
+      |        }
+      |
+      |    }
+      |  }
+      |
+      |}
+      |//--------------------------------------------
+      |""".stripMargin
 }
