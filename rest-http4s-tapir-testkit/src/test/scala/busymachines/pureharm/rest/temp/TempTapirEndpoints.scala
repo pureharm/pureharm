361988f64ad53532c327a8855c19af8d5944511b
Lorand Szakacs <lsz@lorandszakacs.com>
2020-07-16 18:12:17 +0300

    Add missing copyright headers

--- rest-http4s-tapir-testkit/src/test/scala/busymachines/pureharm/rest/temp/TempTapirEndpoints.scala
+++ rest-http4s-tapir-testkit/src/test/scala/busymachines/pureharm/rest/temp/TempTapirEndpoints.scala
@@ -1,3 +1,20 @@
+/**
+  * Copyright (c) 2017-2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package busymachines.pureharm.rest.temp
 
 /**
@@ -6,44 +23,8 @@ package busymachines.pureharm.rest.temp
   */
 object TempTapirEndpoints {
 
-  import busymachines.pureharm.rest._
-
-  val testPath: String = "test"
-
-  //TEMP IMPLEMENTATION
-  //-------------------------------------------------
-  import busymachines.pureharm.anomaly._
   import busymachines.pureharm.effects._
   import busymachines.pureharm.effects.implicits._
-
-  final class TestHttp4sRuntime[F[_]](
-    override val blockingEC:   ExecutionContextCT
-  )(implicit
-    override val F:            Sync[F],
-    override val contextShift: ContextShift[F],
-  ) extends Http4sRuntime[F, Sync[F]] {
-
-    override val http4sServerOptions: Http4sServerOptions[F] =
-      super.http4sServerOptions.withCustomHeaderAuthValidation(AuthTokenHeaderName)
-  }
-
-  object TestHttp4sRuntime {
-
-    def apply[F[_]](ec: ExecutionContextCT)(implicit f: Sync[F], cs: ContextShift[F]): TestHttp4sRuntime[F] =
-      new TestHttp4sRuntime(ec)
-  }
-
-  //using this over Authentication to double as CSRF token as well
-  private lazy val AuthTokenHeaderName = "X-AUTH-TOKEN"
-
-  val authedEndpoint: SimpleEndpoint[MyAuthToken, Unit] =
-    phEndpoint.in(auth.xCustomAuthHeader[MyAuthToken](AuthTokenHeaderName))
-
-  val bearerAuthedEndpoint: SimpleEndpoint[MyAuthToken, Unit] =
-    phEndpoint.in(auth.bearer[MyAuthToken])
-
-  val apiKeyAuthedEndpoint: SimpleEndpoint[MyAuthToken, Unit] =
-    phEndpoint.in(auth.apiKey[MyAuthToken](query[MyAuthToken]("apiKey")))
 
   case class MyAuthContext(token: MyAuthToken)
 
@@ -91,36 +72,69 @@ object TempTapirEndpoints {
       )
   }
 
+  import busymachines.pureharm.rest._
+
+  //using this over Authentication to double as CSRF token as well
+  private lazy val AuthTokenHeaderName = "X-AUTH-TOKEN"
+
+  //----- Create a unified way of doing auth, you can also add in common logic. See tapir docs for that
+  val authedEndpoint: SimpleEndpoint[MyAuthToken, Unit] =
+    phEndpoint.in(auth.xCustomAuthHeader[MyAuthToken](AuthTokenHeaderName))
+
+  //----- Create one of these for your app
+  final class TestHttp4sRuntime[F[_]](
+    override val blockingEC:   ExecutionContextCT
+  )(implicit
+    override val F:            Sync[F],
+    override val contextShift: ContextShift[F],
+  ) extends Http4sRuntime[F, Sync[F]] {
+
+    override val http4sServerOptions: Http4sServerOptions[F] =
+      super.http4sServerOptions.withCustomHeaderAuthValidation(AuthTokenHeaderName)
+  }
+
+  object TestHttp4sRuntime {
+
+    def apply[F[_]](ec: ExecutionContextCT)(implicit f: Sync[F], cs: ContextShift[F]): TestHttp4sRuntime[F] =
+      new TestHttp4sRuntime(ec)
+  }
+
   ////------------------------------------------------
 
+  // ----- Create one of these for your app
   trait MyAppRest[F[_]] extends RestDefs[F, Sync[F], TestHttp4sRuntime[F]]
 
+  // ----- And create as many of these as you need
   final class SomeAPI[F[_]](
     domain:                              SomeOrganizer[F]
   )(implicit override val http4sRuntime: TestHttp4sRuntime[F])
     extends MyAppRest[F] {
 
-    val nonAuthedGetEndpoint: SimpleEndpoint[Unit, MyOutputType] = phEndpoint.get
-      .in("no_auth")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
+    val nonAuthedGetEndpoint: SimpleEndpoint[Unit, MyOutputType] =
+      phEndpoint.get
+        .in("no_auth")
+        .out(jsonBody[MyOutputType])
+        .out(statusCode(StatusCode.Ok))
 
-    val nonAuthedPostEndpoint: SimpleEndpoint[MyInputType, MyOutputType] = phEndpoint.post
-      .in("no_auth")
-      .in(jsonBody[MyInputType])
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Created))
+    val nonAuthedPostEndpoint: SimpleEndpoint[MyInputType, MyOutputType] =
+      phEndpoint.post
+        .in("no_auth")
+        .in(jsonBody[MyInputType])
+        .out(jsonBody[MyOutputType])
+        .out(statusCode(StatusCode.Created))
 
-    val testGetEndpoint: SimpleEndpoint[(MyAuthToken, PHUUID), MyOutputType] = authedEndpoint.get
-      .in("test" / path[PHUUID])
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
+    val testGetEndpoint: SimpleEndpoint[(MyAuthToken, PHUUID), MyOutputType] =
+      authedEndpoint.get
+        .in("test" / path[PHUUID])
+        .out(jsonBody[MyOutputType])
+        .out(statusCode(StatusCode.Ok))
 
-    val testPostEndpoint: SimpleEndpoint[(MyAuthToken, MyInputType), MyOutputType] = authedEndpoint.post
-      .in("test")
-      .in(jsonBody[MyInputType])
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Created))
+    val testPostEndpoint: SimpleEndpoint[(MyAuthToken, MyInputType), MyOutputType] =
+      authedEndpoint.post
+        .in("test")
+        .in(jsonBody[MyInputType])
+        .out(jsonBody[MyOutputType])
+        .out(statusCode(StatusCode.Created))
 
     val testGetEndpointQueryParams: SimpleEndpoint[(MyAuthToken, PHUUID, PHLong, Option[PHInt]), MyOutputType] =
       authedEndpoint.get
@@ -130,63 +144,12 @@ object TempTapirEndpoints {
         .out(jsonBody[MyOutputType])
         .out(statusCode(StatusCode.Ok))
 
-    val testGetWithHeaderEndpoint: SimpleEndpoint[(MyAuthToken, PHUUID, PHHeader), MyOutputType] = authedEndpoint.get
-      .in("test_h" / path[PHUUID])
-      .in(header[PHHeader]("X-TEST-HEADER"))
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testNotFound: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "not_found")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testUnauthorized: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "unauthorized")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testForbidden: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "forbidden")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testDenied: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "denied")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testInvalidInput: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "invalid_input")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testConflict: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "conflict")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testAnomalies: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "anomalies")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testNotImplemented: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "not_implemented")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testCatastrophe: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "catastrophe")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    val testNormalThrowable: SimpleEndpoint[MyAuthToken, MyOutputType] = authedEndpoint.get
-      .in("test_logic" / "throwable")
-      .out(jsonBody[MyOutputType])
-      .out(statusCode(StatusCode.Ok))
-
-    import sttp.tapir.server.http4s._
+    val testGetWithHeaderEndpoint: SimpleEndpoint[(MyAuthToken, PHUUID, PHHeader), MyOutputType] =
+      authedEndpoint.get
+        .in("test_h" / path[PHUUID])
+        .in(header[PHHeader]("X-TEST-HEADER"))
+        .out(jsonBody[MyOutputType])
+        .out(statusCode(StatusCode.Ok))
 
     val nonAuthedGetRoute: HttpRoutes[F] = nonAuthedGetEndpoint.toRouteRecoverErrors { _ =>
       domain.getLogic(PHUUID.unsafeGenerate)(MyAuthToken.unsafeGenerate)
@@ -215,55 +178,6 @@ object TempTapirEndpoints {
       case (auth: MyAuthToken, myInputType: MyInputType) => domain.postLogic(myInputType)(auth)
     }
 
-    val testNotFoundRoute: HttpRoutes[F] = testNotFound.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](NotFoundAnomaly("test_not_found"))
-    }
-
-    val testUnauthorizedRoute: HttpRoutes[F] = testUnauthorized.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](UnauthorizedAnomaly("test_unauthorized"))
-    }
-
-    val testForbiddenRoute: HttpRoutes[F] = testForbidden.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](ForbiddenAnomaly("test_forbidden"))
-    }
-
-    val testDeniedRoute: HttpRoutes[F] = testDenied.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](DeniedAnomaly("test_denied"))
-    }
-
-    val testInvalidInputRoute: HttpRoutes[F] = testInvalidInput.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](InvalidInputAnomaly("test_invalid_input"))
-    }
-
-    val testConflictRoute: HttpRoutes[F] = testConflict.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](ConflictAnomaly("test_conflict"))
-    }
-
-    private case object TestAnomaliesID extends AnomalyID { override val name: String = "test_anomalies" }
-
-    val testAnomaliesRoute: HttpRoutes[F] = testAnomalies.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](
-        Anomalies(
-          TestAnomaliesID,
-          "test_anomalies",
-          ConflictAnomaly("test_conflict"),
-          InvalidInputAnomaly("test_invalid_input"),
-        )
-      )
-    }
-
-    val testNotImplementedRoute: HttpRoutes[F] = testNotImplemented.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](NotImplementedCatastrophe("not_implemented"))
-    }
-
-    val testCatastropheRoute: HttpRoutes[F] = testCatastrophe.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](InconsistentStateCatastrophe("catastrophe"))
-    }
-
-    val testNormalThrowableRoute: HttpRoutes[F] = testNormalThrowable.toRouteRecoverErrors { _ =>
-      F.raiseError[MyOutputType](new RuntimeException("throwable"))
-    }
-
     val routes: HttpRoutes[F] = NEList
       .of[HttpRoutes[F]](
         nonAuthedGetRoute,
@@ -272,19 +186,8 @@ object TempTapirEndpoints {
         testGetEndpointQueryParamsRoute,
         testGetWithHeaderRoute,
         testPostRoute,
-        testNotFoundRoute,
-        testUnauthorizedRoute,
-        testForbiddenRoute,
-        testDeniedRoute,
-        testInvalidInputRoute,
-        testConflictRoute,
-        testAnomaliesRoute,
-        testNotImplementedRoute,
-        testCatastropheRoute,
-        testNormalThrowableRoute,
       )
       .reduceK
-
   }
 
 }
--- rest-http4s-tapir/src/main/scala/busymachines/pureharm/rest/PureharmRestAliasesAndImplicits.scala
+++ rest-http4s-tapir/src/main/scala/busymachines/pureharm/rest/PureharmRestAliasesAndImplicits.scala
@@ -1,3 +1,20 @@
+/**
+  * Copyright (c) 2017-2019 BusyMachines
+  *
+  * See company homepage at: https://www.busymachines.com/
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package busymachines.pureharm.rest
 
 import busymachines.pureharm.internals
@@ -7,6 +24,7 @@ import busymachines.pureharm.internals
   * @since 16 Jul 2020
   */
 trait PureharmRestAliasesAndImplicits
-  extends sttp.tapir.Tapir with internals.rest.PureharmTapirAliases with internals.rest.PureharmTapirModelAliases
-  with internals.rest.PureharmTapirServerAliases with internals.rest.PureharmHttp4sCirceInstances
-  with internals.rest.PureharmRestHttp4sTypeAliases with internals.rest.PureharmRestTapirImplicits {}
+  extends sttp.tapir.Tapir with sttp.tapir.server.http4s.TapirHttp4sServer with internals.rest.PureharmTapirAliases
+  with internals.rest.PureharmTapirModelAliases with internals.rest.PureharmTapirServerAliases
+  with internals.rest.PureharmHttp4sCirceInstances with internals.rest.PureharmRestHttp4sTypeAliases
+  with internals.rest.PureharmRestTapirImplicits {}
