1cb44aeb4d6bf3343cbe10fd0e3a5bfb7ea8d2cf
Lorand Szakacs <lorand.szakacs@gmail.com>
2020-07-30 13:07:07 +0300

    [db-core] remove DBConnectionConfig.scala

--- db/submodules/core/src/main/scala/busymachines/pureharm/db/DBConnectionConfig.scala
+++ db/submodules/core/src/main/scala/busymachines/pureharm/db/DBConnectionConfig.scala
@@ -38,11 +38,7 @@ final case class DBConnectionConfig(
 import busymachines.pureharm.config._
 
 object DBConnectionConfig extends ConfigLoader[DBConnectionConfig] {
-  import busymachines.pureharm.effects.Sync
   import busymachines.pureharm.config.implicits._
 
   implicit override def configReader: ConfigReader[DBConnectionConfig] = semiauto.deriveReader[DBConnectionConfig]
-
-  override def default[F[_]: Sync]: F[DBConnectionConfig] =
-    this.load("pureharm.db.connection")
 }
