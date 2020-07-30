491091c7005324418484343ebae87f80b85ed216
Lorand Szakacs <lorand.szakacs@gmail.com>
2020-07-30 14:22:48 +0300

    [rest] Make Http4sRuntime take a BlockingShifter as param

--- rest-http4s-tapir/src/main/scala/busymachines/pureharm/internals/rest/RestDefs.scala
+++ rest-http4s-tapir/src/main/scala/busymachines/pureharm/internals/rest/RestDefs.scala
@@ -17,7 +17,7 @@
   */
 package busymachines.pureharm.internals.rest
 
-import cats.effect.{ContextShift, Sync}
+import busymachines.pureharm.effects.{BlockingShifter, ContextShift, Sync}
 import sttp.tapir.server.http4s.Http4sServerOptions
 
 /**
@@ -25,9 +25,9 @@ import sttp.tapir.server.http4s.Http4sServerOptions
 trait RestDefs[F[_], ET <: Sync[F], RT <: Http4sRuntime[F, ET]] {
 
   protected def http4sRuntime: RT
-
-  implicit def F:            ET              = http4sRuntime.F
-  implicit def contextShift: ContextShift[F] = http4sRuntime.contextShift
+  implicit def F:               ET                 = http4sRuntime.F
+  implicit def blockingShifter: BlockingShifter[F] = http4sRuntime.blockingShifter
+  implicit def contextShift:    ContextShift[F]    = http4sRuntime.contextShift
 
   implicit def tapirHttp4Ops: Http4sServerOptions[F] = http4sRuntime.http4sServerOptions
 }
